d8112f438f250716697c80b77f85a09c0d2e9802
Muhammad Ihsan Syafiul Umam <80028913+ihsan606@users.noreply.github.com>
2022-07-12 07:34:10 +0700

    add icon in report sales sidebar

--- components/admin/sidebar.vue
+++ components/admin/sidebar.vue
@@ -41,12 +41,20 @@
 
     <li class="c-sidebar-nav-title">REPORTS</li>
 
-     <li class="c-sidebar-nav-item">
+    <li class="c-sidebar-nav-item">
       <nuxt-link :to="{ name: 'admin-invoices' }" class="c-sidebar-nav-link">
-        <svg class="c-sidebar-nav-icon">
-          <use
-            xlink:href="@/node_modules/@coreui/icons/sprites/free.svg#cil-cart"
-          ></use>
+        <svg
+          class="c-sidebar-nav-icon"
+          xmlns="http://www.w3.org/2000/svg"
+          width="16"
+          height="16"
+          fill="currentColor"
+          class="bi bi-bar-chart"
+          viewBox="0 0 16 16"
+        >
+          <path
+            d="M4 11H2v3h2v-3zm5-4H7v7h2V7zm5-5v12h-2V2h2zm-2-1a1 1 0 0 0-1 1v12a1 1 0 0 0 1 1h2a1 1 0 0 0 1-1V2a1 1 0 0 0-1-1h-2zM6 7a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1v7a1 1 0 0 1-1 1H7a1 1 0 0 1-1-1V7zm-5 4a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1v3a1 1 0 0 1-1 1H2a1 1 0 0 1-1-1v-3z"
+          />
         </svg>
         Sales Reports</nuxt-link
       >
